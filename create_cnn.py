c9db3490a4f3ac1335a658967962ad265be349e8
shiryaeva <shiryaeva@haulmont.com>
2021-03-10 11:52:01 +0400

    Prediction works, put your own images in dataset/control_set Still need to improve accuracy (now 70%)

--- create_cnn.py
+++ create_cnn.py
@@ -1,18 +1,19 @@
-# Code taken from https://becominghuman.ai/building-an-image-classifier-using-deep-learning-in-python-totally-from-a-beginners-perspective-be8dbaf22dd8
+# Code mostly taken from
+# https://becominghuman.ai/building-an-image-classifier-using-deep-learning-in-python-totally-from-a-beginners-perspective-be8dbaf22dd8
 # Dataset taken from https://www.kaggle.com/tongpython/cat-and-dog
+
 # Importing the Keras libraries and packages
-from tensorflow import keras
-from keras.models import Sequential
 from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
 from keras.layers import Dense
-from keras.callbacks import ModelCheckpoint
+from keras.layers import Flatten
+from keras.layers import MaxPooling2D
+from keras.models import Sequential
+from tensorflow import keras
 
 # Initialising the CNN
 model = Sequential()
 # Step 1 - Convolution
-model.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3), activation='relu'))
 # Step 2 - Pooling
 model.add(MaxPooling2D(pool_size=(2, 2)))
 # Adding a second convolutional layer
@@ -33,13 +34,16 @@ train_datagen = ImageDataGenerator(rescale=1. / 255,
                                    zoom_range=0.2,
                                    horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+batch_size = 32
+
 training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                 target_size=(64, 64),
-                                                 batch_size=32,
+                                                 target_size=(128, 128),
+                                                 batch_size=batch_size,
                                                  class_mode='binary')
 test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                            target_size=(64, 64),
-                                            batch_size=32,
+                                            target_size=(128, 128),
+                                            batch_size=batch_size,
                                             class_mode='binary')
 
 import os
@@ -47,11 +51,13 @@ import os
 cp_callback = keras.callbacks.ModelCheckpoint(filepath=os.curdir,
                                               save_weights_only=True,
                                               verbose=1)
-model.fit_generator(training_set,
-                    steps_per_epoch=8000 // 32,
-                    epochs=25,
-                    validation_data=test_set,
-                    validation_steps=2000 // 32,
-                    callbacks=[cp_callback])
+
+model.fit(training_set,
+          steps_per_epoch=8000 // batch_size,
+          epochs=25,
+          validation_data=test_set,
+          validation_steps=2000 // batch_size,
+          callbacks=[cp_callback])
+
 model.save("keras_cnn")
 print("Model saved")
--- use_cnn.py
+++ use_cnn.py
@@ -1,74 +1,55 @@
 # Part 3 - Making new predictions
+import os
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator, load_img
-import pandas as pd
-import os
-import matplotlib.pyplot as plt
-
-Image_Width = 128
-Image_Height = 128
-Image_Size = (Image_Width, Image_Height)
-Image_Channels = 3
 
 model = tf.keras.models.load_model("keras_cnn")
 print("Model loaded")
-
-from keras.preprocessing.image import ImageDataGenerator
 
 train_datagen = ImageDataGenerator(rescale=1. / 255,
                                    shear_range=0.2,
                                    zoom_range=0.2,
                                    horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1. / 255)
-
 training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                 target_size=(64, 64),
+                                                 target_size=(128, 128),
                                                  batch_size=32,
                                                  class_mode='binary')
 test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                            target_size=(64, 64),
+                                            target_size=(128, 128),
                                             batch_size=32,
                                             class_mode='binary')
-control_set = test_datagen.flow_from_directory('dataset/control_set',
-                                               target_size=(64, 64),
-                                               batch_size=32,
-                                               class_mode='binary')
-# test_image = image.load_img('dataset/control_set/cat_or_dog_1.jpg', target_size=(64, 64))
-# test_image = image.img_to_array(test_image)
-# test_image = np.expand_dims(test_image, axis=0)
 
-
-control_files = os.listdir("dataset/control_set")
+test_filenames = os.listdir("dataset/control_set")
 test_df = pd.DataFrame({
-    'filename': control_files
+    'filename': test_filenames
 })
 nb_samples = test_df.shape[0]
-directory = 'dataset/control_set'
-predict = model.predict(control_files, steps=np.ceil(nb_samples))
-if predict[0][0] == 1:
-    prediction = 'dog'
-else:
-    prediction = 'cat'
-print(prediction)
 
-test_df['category'] = np.argmax(predict, axis=-1)
-label_map = dict((v, k) for k, v in train_datagen.class_indices.items())
-test_df['category'] = test_df['category'].replace(label_map)
-test_df['category'] = test_df['category'].replace({'dog': 1, 'cat': 0})
+sample_test = test_df.head(18)
+sample_test.head()
 
-control_test = test_df.head(18)
-control_test.head()
-plt.figure(figsize=(12, 24))
-for index, row in control_test.iterrows():
+plt.figure(figsize=(6, 6))
+for index, row in sample_test.iterrows():
     filename = row['filename']
-    category = row['category']
-    img = load_img('dataset/control_set/' + filename, target_size=Image_Size)
-    plt.subplot(6, 3, index + 1)
+    img = load_img("dataset/control_set/" + filename, target_size=(128, 128))
+    test_image = image.load_img("dataset/control_set/" + filename, target_size=(128, 128))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+    result = model.predict(test_image)
+    if result[0][0] == 1:
+        prediction = 'dog'
+    else:
+        prediction = 'cat'
+    category = prediction
+    axes = plt.subplot(6, 3, index + 1)
     plt.imshow(img)
-    plt.xlabel(filename + '(' + "{}".format(category) + ')')
+    plt.xlabel(format(category))
+    axes.set_yticks([])
+    axes.set_xticks([])
 plt.tight_layout()
 plt.show()
-
-
